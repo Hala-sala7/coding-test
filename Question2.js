151f48f426476df75b6f1ac4da04b9d3a707c604
Hala-sala7 <hala.salah1232@gmail.com>
2023-09-14 11:39:24 +0300

    all

---
+++ Question2.js
@@ -0,0 +1,17 @@
+function findMissingNumber(num) {
+    let n = num.length;
+    let expectedResultSum = (n * (n + 1)) / 2;
+    let realSum = num.reduce(function(accumelator, num){
+        return accumelator + num}
+         ,0);
+    return expectedResultSum - realSum;
+  }
+  
+  const nums1 = [3, 0, 1];
+  console.log(findMissingNumber(nums1)); 
+
+  const nums2 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+  console.log(findMissingNumber(nums2)); 
+  
+  const nums3 = [0, 1];
+  console.log(findMissingNumber(nums3)); 
